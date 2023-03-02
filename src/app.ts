43a300ea549404b95c97a944e92911d6ac0760dd
S <popov@diligences.com>
2023-03-02 17:00:57 +0200

    test+

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,16 @@ import { CipherRightShift } from './CipherRightShift';
 import { displayCipherDecipher } from './displayCipherDecipher';
 
 const cipherCode1 = new CipherImpl(4);
+const cipherCode2 = new CipherImpl(-4);
+
 const cls = new CipherLeftShift(4);
-const crs = new CipherRightShift(-4);
+const cls2 = new CipherLeftShift(-4);
+const crs = new CipherRightShift(4);
+const crs2 = new CipherRightShift(-4);
 
 displayCipherDecipher(cipherCode1, "aabx!")
+displayCipherDecipher(cipherCode2, "aabx!")
 displayCipherDecipher(cls, "aabx!")
+displayCipherDecipher(cls2, "aabx!")
 displayCipherDecipher(crs, "aabx!")
+displayCipherDecipher(crs2, "aabx!")
